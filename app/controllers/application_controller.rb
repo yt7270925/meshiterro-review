94e34ec74c70fff8dca5715c988e22f5c248f055
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-07-26 07:12:17 +0000

    ６章 devise(2)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  #devise利用の機能（ログインなど）が使われる前にこのメソッドが実行される
+  before_action :configue_permitted_parameters, if: :devise_controller?
+
+  #他のコントローラからも参照できる
+  protected
+
+    #ユーザー登録（sign_ip）の際に、nameの値を許可
+    #ストロングパラメータと同様の機能
+  def configue_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
