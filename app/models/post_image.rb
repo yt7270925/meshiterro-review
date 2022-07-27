7213fb621c5b56bc19b0b1e52c2b81d843335670
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-07-27 05:38:38 +0000

    14章 userのコントローラ

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,12 +2,12 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  def get_image
+  def get_image(width, height)
     unless
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
